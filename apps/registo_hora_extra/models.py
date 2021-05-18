730df68749f16de69eae312b2c8d4c98faf438fa
RiSantos79 <ricardo.f.santos@sapo.pt>
2021-05-18 23:16:29 +0100

    Criação dos relacionamentos de Base de Dados

--- apps/registo_hora_extra/models.py
+++ apps/registo_hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class RegistoHoraExtra(models.Model):
     motivo = models.CharField(max_length=100, help_text='Motivo')
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
