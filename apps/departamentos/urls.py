3021319dd2cb07deeee4b4df0e6be5455f7f08af
RiSantos79 <ricardo.f.santos@sapo.pt>
2021-05-22 20:16:58 +0100

    CRUD Departamentos, Delete

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     DepartamentosList,
     DepartamentoCreate,
-    DepartamentoUpdate
+    DepartamentoUpdate,
+    DepartamentoDelete
 )
 
 
@@ -10,5 +11,5 @@ urlpatterns = [
     path('list', DepartamentosList.as_view(), name='list_departamentos'),
     path('novo', DepartamentoCreate.as_view(), name='create_departamento'),
     path('update/<int:pk>/', DepartamentoUpdate.as_view(), name='update_departamento'),
-
+    path('delete/<int:pk>/', DepartamentoDelete.as_view(), name='delete_departamento'),
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,7 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Departamento
 
@@ -29,3 +31,7 @@ class DepartamentoUpdate(UpdateView):
     model = Departamento
     fields = ['nome']
 
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
