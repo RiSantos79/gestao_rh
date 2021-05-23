78260d710fd946903d5c92c535b4a2616f1f2b1f
RiSantos79 <ricardo.f.santos@sapo.pt>
2021-05-23 15:21:20 +0100

    CRUD de Documentos

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Documento
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class DocumentoCreate(CreateView):
+    model = Documento
+    fields = ['descricao', 'arquivo']
+
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -93,8 +93,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
